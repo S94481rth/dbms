38fbec8ed930a9ce31535b5df5860741d6cb868b
S94481rth <93481116+S94481rth@users.noreply.github.com>
2022-08-15 23:39:09 +0530

    Create a.sql

---
+++ a.sql
@@ -0,0 +1,52 @@
+create table stores(
+  id int primary key,
+  loc varchar(10),
+  storeName varchar(10)
+);
+
+create table customer(
+  cid int primary key,
+  locCust varchar(10),
+  firstOrder varchar(10)
+  );
+
+create table item(
+	  itemNumber int primary key,
+    itemName varchar(10)
+);
+
+create table hdqADD(
+    itemNumber int,
+	  city varchar(10) primary key,
+    quantity int,
+    foreign key(itemNumber) REFERENCES item(itemNumber)
+);
+
+create table orders(
+	  orderNumber int primary key,
+    cid int,
+    sid int,
+    itemNumber int,
+    foreign key(cid) REFERENCES customer(cid),
+    foreign key(sid) REFERENCES stores(id),
+    foreign key(itemNumber) REFERENCES item(itemNumber)
+);
+
+
+create table itemOrdered(
+	  itemNumber int,
+    orderNumber int,
+    quantity int,
+    primary key(itemNumber,orderNumber),
+    FOREIGN key(itemNumber) REFERENCES item(itemNumber),
+    FOREIGN key(orderNumber) REFERENCES orders(orderNumber)
+);
+
+create table holds(
+	  sid int,
+    itemNumber int,
+    quantity int,
+    primary key(sid,itemNumber),
+    FOREIGN key(sid) references stores(id),
+    FOREIGN key(itemNumber) references item(itemNumber)
+);
